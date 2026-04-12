3761b54c513870e6c25ab283ef5c8bf08a1d44c5
Odin Sandlie <odinsm@stud.ntnu.no>
2026-04-12 17:15:21 +0200

    train and test function for training on the first 30 samples

--- iris_task/iris_training_model.py
+++ iris_task/iris_training_model.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+np.random.seed(42)
+
 
 class LCD_training_model:
 
@@ -21,6 +23,7 @@ class LCD_training_model:
         #Generate weight matrix. Can either be initialized to zero, or with random values
         if random_initial_weight_matrix:
             self.W = np.random.rand(number_of_input_variables,number_of_classes)
+            #self.W = 0.01 * np.random.randn(number_of_input_variables, number_of_classes)
         else:
             self.W = np.zeros((number_of_input_variables, number_of_classes))
 
@@ -55,9 +58,10 @@ class LCD_training_model:
     #     return np.dot(wi_T,x) + wio
 
     def linear_discriminant(self,x):
+        #return np.matmul(self.W.T, x)
         W_T = np.transpose(self.W)
 
-        return np.matmul(W_T,x)
+        return np.matmul(W_T,x) #DxC?
     
     def MSE(self,gk,tk):
 
@@ -108,6 +112,7 @@ class LCD_training_model:
                 MSE_grad_k += self.MSE_gradient(gk,tk,xk)
 
 
+
             #update weight matrix
             self.update_W(MSE_grad_k)
 
@@ -115,17 +120,90 @@ class LCD_training_model:
             self.MSE_vector[index] = MSE_k
             self.MSE_gradient_vector[index] = np.linalg.norm(MSE_grad_k)
             
-            #Prepare for next iteration
-            self.MSE_k = 0
-            self.MSE_grad_k = np.zeros_like(self.W)
-            index += 1
 
             
 #testing function
-    def test(self):
-        pass
+    def test(self, dataset):
+        correct = 0
+        total = len(dataset)
+        confusion_matrix = np.zeros((self.number_of_classes, self.number_of_classes), dtype=int)
 
-    
+        for xk, label in dataset:
+            z = self.linear_discriminant(xk)
+            gk = self.sigmoid(z)
 
-        
-        
+            predicted_index = np.argmax(gk)
+            true_index = self.label_to_target_index(label)
+
+            confusion_matrix[true_index, predicted_index] += 1
+
+            if predicted_index == true_index:
+                correct += 1
+
+        error_count = total - correct
+        error_rate = error_count / total
+
+        return correct, error_count, error_rate, confusion_matrix
+
+
+df = pd.read_csv(
+    "iris_data_sets/iris.csv",
+    header=None
+)
+
+class_labels = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
+
+class_1 = df.iloc[0:50]
+class_2 = df.iloc[50:100]
+class_3 = df.iloc[100:150]
+
+train_df = pd.concat([class_1.iloc[0:30], class_2.iloc[0:30], class_3.iloc[0:30]])
+test_df = pd.concat([class_1.iloc[30:50], class_2.iloc[30:50], class_3.iloc[30:50]])
+
+
+def dataframe_to_dataset(dataframe):
+    dataset = []
+
+    for _, row in dataframe.iterrows():
+        xk = row.iloc[0:4].to_numpy(dtype=float)
+        label = row.iloc[4]
+        dataset.append((xk, label))
+
+    return dataset
+
+
+training_set = dataframe_to_dataset(train_df)
+testing_set = dataframe_to_dataset(test_df)
+
+
+model = LCD_training_model(
+    training_set=training_set,
+    testing_set=testing_set,
+    number_of_classes=3,
+    number_of_input_variables=4,
+    class_labels=class_labels,
+    alpha=0.01,
+    iterations=1000,
+    random_initial_weight_matrix=True
+)
+
+model.train()
+
+train_correct, train_error_count, train_error_rate, train_confusion_matrix = model.test(training_set)
+test_correct, test_error_count, test_error_rate, test_confusion_matrix = model.test(testing_set)
+
+print("Training set results:")
+print("Correct classifications:", train_correct)
+print("Misclassifications:", train_error_count)
+print("Error rate:", train_error_rate)
+print("Confusion matrix:")
+print(train_confusion_matrix)
+
+print()
+
+print("Testing set results:")
+print("Correct classifications:", test_correct)
+print("Misclassifications:", test_error_count)
+print("Error rate:", test_error_rate)
+print("Confusion matrix:")
+print(test_confusion_matrix)
