18a3a2e190ff6a86e0298ea5947254688aabc800
Mathilde <mathilde.skaset.haar@gmail.com>
2026-04-17 19:59:28 +0200

    More plots

--- MNIST_task/MNIST_tasks.py
+++ MNIST_task/MNIST_tasks.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 from pathlib import Path
 
-def solve_task_one(folder_path=None,figure_names=None, save_figures=False):
+def solve_task_one(k_neighbors=1,folder_path=None,figure_names=None, save_figures=False):
 
     mnist_klassefier =  MNIST_Classefier(file_path ='MNIST_task/NMIST_data_sets/data_all.mat', 
                                                      num_classes=10, 
@@ -12,7 +12,7 @@ def solve_task_one(folder_path=None,figure_names=None, save_figures=False):
                                                      n_clusters=64, 
                                                      chunk_size=10000)
     
-    time, error_rate, failed_predictions, successfull_predictions = mnist_klassefier.run_KNN(print_progress_updates=True,slow = True)
+    time, error_rate, failed_predictions, successfull_predictions = mnist_klassefier.run_KNN(k_neighbors= k_neighbors,print_progress_updates=True,slow = True)
 
     intersting_misclassifications = [[5,3], [3,8], [1, 7], [9,4]]
     intersting_correctclassifications = [[1,1], [0,0], [2,2], [8,8]]
@@ -106,13 +106,13 @@ def solve_task_two(folder_path=None,figure_names=None, save_figures=False, k_nei
         with open(os.path.join(destination_folder, txt_filname), 'w') as f:
             f.write(f'Error rate: {error_rate}\n')
             f.write(f'Failed predictions: {len(failed_predictions)}\n')
-            f.write(f'Total Classification time: {time[0]}\n s')
-            f.write(f'Cluster time:  {time[1]}\n s')
+            f.write(f'Total Classification time: {time[0]} s\n')
+            f.write(f'Cluster time:  {time[1]} s \n')
             f.write(f'Classification (wo. clustertime): {time[0]+time[1]} s\n')
 
     return error_rate
 
-folderPath = 'test_again_6' 
+folderPath = 'test_again_7' 
 
 fig_labels_task1 = ['cm_t1.pdf',
                     'cm_PCR_t1.pdf',
@@ -129,6 +129,17 @@ fig_labels_task2B = ['cm_2B.pdf',
                      'f_fig_2B.pdf',
                      's_fig_2B.pdf']
 
-error_1 = solve_task_one(folder_path = folderPath ,figure_names=fig_labels_task1,save_figures=True )
-error_2A = solve_task_two(folder_path = folderPath ,figure_names=fig_labels_task2A,save_figures=True)
-error_2B = solve_task_two(k_neighbors=7, folder_path = folderPath ,figure_names=fig_labels_task2B,save_figures=True )
+error_1 = solve_task_one(k_neighbors=7,
+                         folder_path = folderPath,
+                         figure_names=fig_labels_task1,
+                         save_figures=True )
+
+
+error_2A = solve_task_two(folder_path = folderPath,
+                          figure_names=fig_labels_task2A,
+                          save_figures=True)
+
+error_2B = solve_task_two(k_neighbors=7, 
+                          folder_path = folderPath,
+                          figure_names=fig_labels_task2B,
+                          save_figures=True )
